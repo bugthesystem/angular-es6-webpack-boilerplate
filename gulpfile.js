1d897ab2625fe1a5fc970f54ad02d6484dcdebab
ziyasal <sarikayaziya@gmail.com>
2016-01-02 17:35:38 +0200

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-﻿var gulp = require('gulp');
+﻿
+var gulp = require('gulp');
 var templateCache = require('gulp-angular-templatecache');
 var gutil = require('gulp-util');
 
@@ -11,55 +12,40 @@ webpackDevCfg.devtool = 'sourcemap';
 webpackDevCfg.debug = true;
 var devCompiler = webpack(webpackDevCfg);
 
-gulp.task('tpl-cache', function () {
-    return gulp.src('app/**/*html')
-        .pipe(templateCache({
-            standalone: true
-        }))
-        .pipe(gulp.dest('app'));
+gulp.task('tpl-cache', function() {
+  return gulp.src('app/**/*html')
+    .pipe(templateCache({
+      standalone: true
+    }))
+    .pipe(gulp.dest('app'));
 });
 
-gulp.task('watch:html', function () {
-    return gulp.watch(['app/**/*.html'], ['tpl-cache']);
-});
-
-
-gulp.task('build-dev', ['tpl-cache', 'webpack:build-dev'], function () {
-    gulp.watch(['app/**/*'], ['tpl-cache', 'webpack:build-dev']);
-});
-
-gulp.task('webpack:dev-server', function (callback) {
-
-    new WebpackDevServer(devCompiler, {
-        contentBase: 'public',
-        hot: true,
-        filename: 'bundle.js',
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: 1000
-        }
-    }).listen(8080, 'localhost', function (err) {
-        if (err) throw new gutil.PluginError('webpack-dev-server', err);
-
-        gutil.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html');
-
-        callback();
-    });
-});
-
-gulp.task('webpack:build-dev', function (callback) {
-    devCompiler.run(function (err, stats) {
-        if (err) throw new gutil.PluginError('webpack:build-dev', err);
-        gutil.log('[webpack:build-dev]', stats.toString({
-            colors: true
-        }));
-        callback();
-    });
+gulp.task('watch:html', function() {
+  return gulp.watch(['app/**/*.html'], ['tpl-cache']);
 });
 
 
-gulp.task('json-server', function () {
-    require('./index');
+gulp.task('webpack:dev-server', function(callback) {
+
+  new WebpackDevServer(devCompiler, {
+    contentBase: 'public',
+    hot: true,
+    filename: 'bundle.js',
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  }).listen(8080, 'localhost', function(err) {
+    if (err) throw new gutil.PluginError('webpack-dev-server', err);
+
+    gutil.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html');
+
+    callback();
+  });
+});
+
+gulp.task('json-server', function() {
+  require('./index');
 });
 
 gulp.task('default', ['json-server', 'watch:html', 'webpack:dev-server']);
